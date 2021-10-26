76ba3a894c491dc05776f46854a26da4a51d6afe
Chottek <49697145+Chottek@users.noreply.github.com>
2021-10-26 21:01:13 +0200

    Implemented pipes to be correctly spawned

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    private const float PIPE_WIDTH = 1.56f;
+    private const float CAMERA_ORTHO_SIZE = 50f;
+    private const float PIPE_WIDTH = 9f;
     private const float PIPE_HEAD_HEIGHT = 0.75f;
 
     private void Start() {
-        CreatePipe(50f, 0f);
-        CreatePipe(50f, 20f);
+        CreatePipe(40f, 20f, true);
+        CreatePipe(40f, 20f, false);
     }
 
-    private void CreatePipe(float height, float xPosition) {
+    private void CreatePipe(float height, float xPosition, bool bottom) {
         Transform pipeHead = Instantiate(GameAssets.GetInstance().pfPipeHead);
-        pipeHead.position = new Vector3(xPosition, height - (PIPE_HEAD_HEIGHT / 2f));
+        pipeHead.position = new Vector3(xPosition, -CAMERA_ORTHO_SIZE + height - (PIPE_HEAD_HEIGHT / 2f));
+        
+        pipeHead.position = new Vector3(
+            xPosition,
+            bottom ? -CAMERA_ORTHO_SIZE + height - (PIPE_HEAD_HEIGHT / 2f) : CAMERA_ORTHO_SIZE - height + (PIPE_HEAD_HEIGHT / 2f)
+        );
 
         Transform pipeBody = Instantiate(GameAssets.GetInstance().pfPipeBody);
-        pipeBody.position = new Vector3(xPosition, 0f);
+        float pipeBodyYPos;
+        if(bottom){
+            pipeBodyYPos = -CAMERA_ORTHO_SIZE;
+        }else{
+            pipeBodyYPos = CAMERA_ORTHO_SIZE;
+            pipeBody.localScale = new Vector3(1, -1, 1);
+        }
+        pipeBody.position = new Vector3(xPosition, pipeBodyYPos);
 
         SpriteRenderer pipeBodySpriteRenderer = pipeBody.GetComponent<SpriteRenderer>();
         pipeBodySpriteRenderer.size = new Vector2(PIPE_WIDTH, height);
+
+        BoxCollider2D pipeBodyBoxCollider = pipeBody.GetComponent<BoxCollider2D>();
+        pipeBodyBoxCollider.size = new Vector2(PIPE_WIDTH, height);
+        pipeBodyBoxCollider.offset = new Vector2(xPosition, height / 2f);
     }
 }
