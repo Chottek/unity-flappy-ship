5fb0b34298d003b24e46975d4265e924ec1bd750
Chottek <49697145+Chottek@users.noreply.github.com>
2021-10-28 21:54:28 +0200

    Added Game Over menu with retry button

---
+++ Assets/Scripts/GameOverWindow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Hotware.Utils;
+
+public class GameOverWindow : MonoBehaviour {
+
+	private Text scoreText;
+
+	private void Awake() {
+		scoreText = transform.Find("scoreText").GetComponent<Text>();
+
+		transform.Find("retryBtn").GetComponent<UI_Btn>().ClickFunc = () => {
+			UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+		};
+	}
+
+	private void Start() {
+		Ship.GetInstance().OnDeath += Ship_OnDeath;
+		Hide();
+	}
+
+	private void Ship_OnDeath(object sender, System.EventArgs e) {
+		scoreText.text = Level.GetInstance().GetPipesPassedCount().ToString();
+		Show();
+	}
+
+	private void Hide() {
+		gameObject.SetActive(false);
+	}
+
+	private void Show() {
+		gameObject.SetActive(true);
+	}
+   
+}
